6352ef1dfd732381ca18c0b17056d8c64dd9f8dc
RoyiKatz <katzroy@post.bgu.ac.il>
2023-05-28 15:27:13 +0300

    Create Employee.java

---
+++ src/Employee.java
@@ -0,0 +1,18 @@
+
+public abstract class Employee {
+
+	protected int id;
+	protected int rate;
+	
+	
+	// constructors
+	public Employee(int id, int rate) {
+		this.id = id;
+		this.rate = rate;
+	}
+	
+	public Employee(int id) {
+		this.id = id;
+	}
+	
+}
