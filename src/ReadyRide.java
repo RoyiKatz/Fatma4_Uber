484495cfb56884ad3ed08b45a72f3e91515281e4
RoyiKatz <katzroy@post.bgu.ac.il>
2023-05-28 14:39:05 +0300

    Create ReadyRide.java

---
+++ src/ReadyRide.java
@@ -0,0 +1,14 @@
+
+public class ReadyRide {
+
+	private ServiceCall order;
+	private Vehicle vehicle;
+	
+	
+	// constructor
+	public ReadyRide(ServiceCall call, Vehicle v) {
+		order = call;
+		vehicle = v;
+	}
+	
+}
