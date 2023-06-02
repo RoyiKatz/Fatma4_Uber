c840d84d8733290443e871e131cd2f23bd4d563a
RoyiKatz <katzroy@post.bgu.ac.il>
2023-06-02 11:55:25 +0300

    minor updates

--- src/InformationSystem.java
+++ src/InformationSystem.java
@@ -1,43 +1,41 @@
 
 public class InformationSystem {
-	
+
 	private UnboundedBuffer<ServiceCall> delivery_calls, taxi_calls; //check for better way
-	
-	
+
+
 	// constructor
 	public InformationSystem() {
 		delivery_calls = new UnboundedBuffer<ServiceCall>();
 		taxi_calls = new UnboundedBuffer<ServiceCall>();
 	}
-	
-	
+
+
 	// add call
 	public synchronized void addCall(ServiceCall call) {
-		
+
 		if (call.type().equals("Taxi")) {
-			taxi_calls.add(call);
+			taxi_calls.insert(call);
 		} else {
-			delivery_calls.add(call);
+			delivery_calls.insert(call);
 		}
-		
-		// print message
-		
+
 	}
-	
-	
+
+
 	// getters
 	public UnboundedBuffer<ServiceCall> deliveryCalls(){
 		return delivery_calls;
 	}
-	
+
 	public UnboundedBuffer<ServiceCall> taxiCalls(){
 		return taxi_calls;
 	}
-	
-	
+
+
 	// is empty
 	public boolean isEmpty() {
 		return delivery_calls.isEmpty() && taxi_calls.isEmpty();
- 	}
-	
+	}
+
 }
