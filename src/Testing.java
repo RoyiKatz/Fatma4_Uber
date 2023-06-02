8a66e7f3a2d60adff0e8df44cc196dfd6cb67827
RoyiKatz <katzroy@post.bgu.ac.il>
2023-06-02 12:09:31 +0300

    Test

--- src/Testing.java
+++ src/Testing.java
@@ -45,11 +45,27 @@ public class Testing {
 		UnboundedBuffer<Request> special_requests = new UnboundedBuffer<Request>();
 		UnboundedBuffer<ServiceCall> calls = new UnboundedBuffer<ServiceCall>();
 		Vector<Customer> customers = new Vector<Customer>();
+		InformationSystem center = new InformationSystem();
 		
 		//initializing employees
-		Clerk c1 = new Clerk(0, requests, special_requests, calls, customers);
+		Clerk c1 = new Clerk(1, requests, special_requests, calls, customers);
+		Clerk c2 = new Clerk(2, requests, special_requests, calls, customers);
+		Clerk c3 = new Clerk(3, requests, special_requests, calls, customers);
+		Scheduler s1 = new Scheduler(1, "Tel Aviv", calls, center);
+		Scheduler s2 = new Scheduler(2, "Jerusalem", calls, center);
+
 		Thread clerk1 = new Thread(c1);
+		Thread clerk2 = new Thread(c2);
+		Thread clerk3 = new Thread(c3);
+		Thread scheduler1 = new Thread(s1);
+		Thread scheduler2 = new Thread(s2);
+		
 		clerk1.start();
+		clerk2.start();
+		clerk3.start();
+		scheduler1.start();
+		scheduler2.start();
+		
 		
 		//reading requests
 		Vector<String> req_txt = readRequests();
