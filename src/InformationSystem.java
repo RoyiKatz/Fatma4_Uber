b57e83b8af2f12c117d08c09db7421615b9af524
RoyiKatz <katzroy@post.bgu.ac.il>
2023-06-04 14:29:37 +0300

    popular area added

--- src/InformationSystem.java
+++ src/InformationSystem.java
@@ -14,8 +14,10 @@ public class InformationSystem {
 		deliveries = 0;
 		taxi_rides = 0;
 		
-		service_area_counter = new HashMap<String, Integer>();
 		// initialize map
+		service_area_counter = new HashMap<String, Integer>();
+		service_area_counter.put("Tel Aviv", 0);
+		service_area_counter.put("Jerusalem", 0);
 	}
 	
 	
@@ -30,7 +32,12 @@ public class InformationSystem {
 
 
 	// add call
-	public void addCall(Ride call) {
+	public synchronized void addCall(Ride call) {
+		
+		// update service area counter
+		String area = call.details().area();
+		int occurence = service_area_counter.get(area);
+		service_area_counter.replace(area, occurence + 1);
 
 		if (call.details().type().equals("Taxi")) {
 			taxi_calls.insert(call);
@@ -40,22 +47,37 @@ public class InformationSystem {
 			deliveries++;
 		}
 		
+		this.notifyAll();
 	}
 
 	
 	// extract a service call from a given list (Delivery/Taxi)
 	public Ride extract(String list) throws InterruptedException {
-
 		return (list.equals("Taxi")) ? taxi_calls.extract() : delivery_calls.extract();
 	}
 
 	
+	// return whether a specific queue of rides is empty
 	public boolean isEmpty(String buffer) {
 		if (buffer.equals("Taxi")) {
 			return taxi_calls.isEmpty();
 		} else {
 			return delivery_calls.isEmpty();
 		}
+	}
+	
+	// return whether both rude queues are empty
+	public boolean isEmpty() {
+		return taxi_calls.isEmpty() && delivery_calls.isEmpty();
+	}
+	
+	
+	// return the most popular area
+	public String mostPopularArea() {
+		int tel_aviv = service_area_counter.get("Tel Aviv");
+		int jerusalem = service_area_counter.get("Jerusalem");
+		
+		return (tel_aviv > jerusalem) ? "Tel Aviv": "Jerusalem";
 	}
 
 }
